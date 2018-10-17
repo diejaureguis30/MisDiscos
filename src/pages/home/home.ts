4594264820ae034bcb1b5a44e3ad99e161675df8
Diejaureguis30 <jauregui.diego01@gmail.com>
2018-10-17 08:22:39 -0500

    album image added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlbumPage } from '../album/album';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  alb=AlbumPage;
 
   constructor(public navCtrl: NavController) {
 
+  }
+  ClickEm(){
+    this.navCtrl.push(this.alb);
   }
 
 }
