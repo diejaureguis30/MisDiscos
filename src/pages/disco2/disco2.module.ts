376b882b85c85cbdb4fe0e10fdfa0ec90e0423a8
Diejaureguis30 <jauregui.diego01@gmail.com>
2018-10-22 10:25:29 -0500

    Creacion branch 2, paginas

---
+++ src/pages/disco2/disco2.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { Disco2Page } from './disco2';
+
+@NgModule({
+  declarations: [
+    Disco2Page,
+  ],
+  imports: [
+    IonicPageModule.forChild(Disco2Page),
+  ],
+})
+export class Disco2PageModule {}
---
+++ src/pages/soundtrack2/soundtrack2.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { Soundtrack2Page } from './soundtrack2';
+
+@NgModule({
+  declarations: [
+    Soundtrack2Page,
+  ],
+  imports: [
+    IonicPageModule.forChild(Soundtrack2Page),
+  ],
+})
+export class Soundtrack2PageModule {}
