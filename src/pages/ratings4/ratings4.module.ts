ccc57940809120a48c3a5614c0cc1f870d540c0d
Diejaureguis30 <jauregui.diego01@gmail.com>
2018-10-23 09:49:18 -0500

    Creacion Disco 4

---
+++ src/pages/ratings4/ratings4.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { Ratings4Page } from './ratings4';
+
+@NgModule({
+  declarations: [
+    Ratings4Page,
+  ],
+  imports: [
+    IonicPageModule.forChild(Ratings4Page),
+  ],
+})
+export class Ratings4PageModule {}
---
+++ src/pages/soundtrack4/soundtrack4.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { Soundtrack4Page } from './soundtrack4';
+
+@NgModule({
+  declarations: [
+    Soundtrack4Page,
+  ],
+  imports: [
+    IonicPageModule.forChild(Soundtrack4Page),
+  ],
+})
+export class Soundtrack4PageModule {}
